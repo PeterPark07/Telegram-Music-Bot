ebb04b05fa8103e77c00253b1d7774b743a619a1
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-18 14:31:21 +0530

    Update telegram.py

--- helper/telegram.py
+++ helper/telegram.py
@@ -15,13 +15,13 @@ def sendMessage(sender_id: int, message: str) -> None:
 
     requests.request("POST", url, json=payload, headers=headers)
 
-def sendAudio(chat_id: int, audio_file: str, title: str = None) -> None:
+def sendAudio(sender_id: int, audio_file: str, title: str = None) -> None:
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_API_KEY}/sendAudio"
     
     payload = {
-        "chat_id": chat_id,
+        "chat_id": sender_id,
         "audio": audio_file,
+        "title": title
     }
-    headers = {"Content-Type": "application/json"}
-
-    requests.request("POST", url, json=payload, headers=headers)
+    
+    requests.post(url, data=payload)
