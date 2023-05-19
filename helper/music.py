1560a64e1567b338c2fe810955c64c835dfed06a
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-19 18:27:46 +0530

    Update music.py

--- helper/music.py
+++ helper/music.py
@@ -11,17 +11,18 @@ def search(query):
     search = VideosSearch(query, limit=1)
     results = search.result().get('result')
     if not results:
-        return "No videos found for that query.", None, None, None
+        return "No videos found for that query.", None, None, None, None
 
     selected_video = results[0]
     if selected_video['duration'] is None:
-        return "Give a more relevant query.", None, None, None
+        return "Give a more relevant query.", None, None, None, None
 
     url = selected_video['link']
     title = selected_video['title']
     duration = selected_video['duration']
     thumbnail = selected_video['thumbnails'][0]['url']
-    return title, url, duration, thumbnail
+    thumbnail2 = selected_video['richThumbnail'][0]['url']
+    return title, url, duration, thumbnail , thumbnail2
 
 
 def download_audio(url):
