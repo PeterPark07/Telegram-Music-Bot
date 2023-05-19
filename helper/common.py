8f930aa114e9ae6102d374d83168d45304ea9439
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-20 00:00:27 +0530

    Create common.py

---
+++ helper/common.py
@@ -0,0 +1,16 @@
+start = "Hello there! I am MusicBot, your personal music assistant.\n To find any song or audio, simply send me the title you want to search for."
+Help = "MusicBot Help: \n\nSend me the title or description of a song or audio you want to find, and I will fetch it for you."
+on = "BOT ON"
+off = "BOT OFF"
+
+def commands(text):
+    if text == '/start':
+        return start 
+    if text = '/help':
+        return Help 
+    if text = '/on':
+        return on 
+    if text = '/off':
+        return off
+    else:
+        return 0
