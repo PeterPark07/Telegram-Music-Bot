86c9bd8dd7ccd8ccda8450f147cee9baf48da271
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-19 20:07:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,28 +1,24 @@
 import os
 from flask import Flask, request
 from telegram.bot import Bot
-
-from helper.music import search , download_audio
+from helper.music import search, download_audio
 
 app = Flask(__name__)
-
-# Create a Telegram bot
 bot = Bot(os.getenv('TELEGRAM_BOT'))
 
 @app.route('/download', methods=['POST'])
 def telegram():
-
     message = request.get_json()['message']
     sender_id = message['from']['id']
     text = message['text']
     
-    title , url , duration , thumbnail, thumbnail2= search(text)
+    title, url, duration, thumbnail, thumbnail2 = search(text)
     
     if not url:
         bot.send_message(sender_id, title)
-        return 'Fail' , 200
+        return 'Fail', 200
     
-    message_text = f"{title}\n\n {duration}\n\n {url}"
+    message_text = f"{title}\n\n{duration}\n\n{url}"
     bot.send_message(sender_id, message_text)
     
     try:
@@ -35,12 +31,12 @@ def telegram():
     
     response, audio_file = download_audio(url)
     
-    if not audio_file :
+    if not audio_file:
         bot.send_message(sender_id, response)
-        return 'Fail' , 200
+        return 'Fail', 200
 
     with open(audio_file, 'rb') as f:
-        bot.send_audio(sender_id, f , caption = title)
+        bot.send_audio(sender_id, f, caption=title)
     
     return 'OK', 200
 
