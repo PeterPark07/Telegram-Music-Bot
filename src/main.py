206d0a37860b9d1d2383803d26bc52ed3bb2e010
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-26 17:55:58 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -44,7 +44,31 @@ def handle_off(message):
     # Handle the /off command
     bot.reply_to(message, "BOT OFF")
 
+    
+# Store the user's preferred audio format
+user_audio_format = 'best'  # Default value
+    
+@bot.message_handler(commands=['settings'])
+def settings(message):
+    # Create an inline keyboard for audio format selection
+    keyboard = types.InlineKeyboardMarkup()
 
+    # Create inline keyboard buttons for each audio format
+    formats = ['best', 'aac', 'alac', 'flac', 'm4a', 'mp3', 'opus', 'vorbis', 'wav']
+    buttons = [types.InlineKeyboardButton(format_name, callback_data=format_name) for format_name in formats]
+
+    # Add buttons to the keyboard
+    keyboard.add(*buttons)
+
+    # Send a message with the inline keyboard
+    bot.send_message(message.chat.id, "Select your desired audio format:", reply_markup=keyboard)
+    
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    global user_audio_format
+    user_audio_format = call.data
+    bot.send_message(call.message.chat.id, f"Audio format set to {call.data}")
+    
 @bot.message_handler(func=lambda message: True)
 def handle_other_messages(message):
     if not state :
@@ -60,7 +84,7 @@ def handle_other_messages(message):
     else:
         # Download audio file
         wait = bot.reply_to(message, 'Downloading...')
-        response, audio_file, thumbnail = download_audio(url)
+        response, audio_file, thumbnail = download_audio(url , user_audio_format)
         bot.delete_message(message.chat.id, wait.message_id)
 
         if not audio_file:
