c3d6b96c2a9158815390276cb7b815026873c411
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-28 13:32:37 +0530

    Update log.py

--- helper/log.py
+++ helper/log.py
@@ -2,13 +2,13 @@ import time
 import os
 
 log_chat = os.getenv('log_chat')
-def send_log(message):
+def send_log(bot , message):
 
     name = message.from_user.username or message.from_user.first_name
     chat_id = message.chat.id
     message_text = message.text
 
-    log_message = f"Bot: Music\n" \
+    log_message = f"Bot: @{bot.get_me().username}\n" \
                   f"Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n" \
                   f"User: {name}\n" \
                   f"Chat ID: {chat_id}\n" \
