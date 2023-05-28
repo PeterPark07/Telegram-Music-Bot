de583e61c065b00c3b71a95ef459e3e218aa52c0
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-28 13:09:39 +0530

    Create log.py

---
+++ helper/log.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+log_chat = os.getenv('log_chat')
+def send_log(message):
+
+    name = message.from_user.username or message.from_user.first_name
+    chat_id = message.chat.id
+    message_text = message.text
+
+    log_message = f"Bot: Music\n" \
+                  f"Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n" \
+                  f"User: {name}\n" \
+                  f"Chat ID: {chat_id}\n" \
+                  f"Message: {message_text}"
+
+    bot.send_message(log_chat, log_message)
