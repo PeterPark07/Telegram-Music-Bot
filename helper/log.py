87814ccb7fb6e0a65135b47611d35d67407f5a56
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-30 23:57:55 +0530

    Update log.py

--- helper/log.py
+++ helper/log.py
@@ -1,24 +1,24 @@
-import os
-
-log_chat = os.getenv('log_chat')
-admin_user = [int(id) for id in (os.getenv('admin').split(','))][0]
-
-def send_log(bot, message):
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    message_text = message.text
-    name = message.from_user.username or message.from_user.first_name
-
-    # Determine the information to display based on user and chat IDs
-    if user_id == admin_user:
-        log_info = "Admin , "
-    elif user_id == chat_id:
-        log_info = f"User: {name}\nChat ID: {chat_id}\n"
-    else:
-        log_info = f"User: {name}\nUser ID: {user_id}\nChat ID: {chat_id}\n"
-
-    # Construct the log message with relevant details
-    log_message = f"Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}"
-
-    # Send the log message to the designated log chat
-    bot.send_message(log_chat, log_message)
+import os 
+  
+ log_chat = os.getenv('log_chat') 
+ admin_user = int(os.getenv('admin')) 
+  
+ def send_log(bot, message): 
+     user_id = message.from_user.id 
+     chat_id = message.chat.id 
+     message_text = message.text 
+     name = message.from_user.username or message.from_user.first_name 
+  
+     # Determine the information to display based on user and chat IDs 
+     if user_id == admin_user: 
+         log_info = "Admin , " 
+     elif user_id == chat_id: 
+         log_info = f"User: {name}\nChat ID: {chat_id}\n" 
+     else: 
+         log_info = f"User: {name}\nUser ID: {user_id}\nChat ID: {chat_id}\n" 
+  
+     # Construct the log message with relevant details 
+     log_message = f"Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}" 
+  
+     # Send the log message to the designated log chat 
+     bot.send_message(log_chat, log_message)
